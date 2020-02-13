ecae622e8e4f08e78f06c594c9202a1a361e0de9
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-02-13 12:43:03 +0100

    migrate and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "minth")
+Ingredient.create(name: "beer")
+Ingredient.create(name: "whisky")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "martini")
+Ingredient.create(name: "cachaça")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "grapefruit")
+Ingredient.create(name: "cranberries")
+Ingredient.create(name: "mint leaves")
